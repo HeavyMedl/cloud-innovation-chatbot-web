eff4a30dd91f1e93a9eca3f57ed8c390c1b5f556
Kurt Medley <kurtdmedley@gmail.com>
2019-06-11 14:26:33 -0700

    Changing the homepage text to see if Azure auto-deploys with git push

--- src/App.js
+++ src/App.js
@@ -4,17 +4,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <p>If I push to github, will it deploy to Azure?</p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
