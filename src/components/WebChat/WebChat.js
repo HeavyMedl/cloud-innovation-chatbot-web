977e600d89259ca7a91d989b27a533fc55dafa54
Jana Uhrich <janamu@uw.edu>
2019-06-11 16:49:12 -0700

    craig will answer you now

--- src/components/WebChat/WebChat.js
+++ src/components/WebChat/WebChat.js
@@ -1,6 +1,11 @@
 import { DirectLine } from 'botframework-directlinejs';
 import React from 'react';
 import ReactWebChat from 'botframework-webchat';
+import craigPic from '../../images/craig-jelinek.png'
+
+const styleOptions = {
+  botAvatarImage: craigPic
+ }
 
 export default class extends React.Component {
   constructor(props) {
@@ -8,9 +13,10 @@ export default class extends React.Component {
     this.directLine = new DirectLine({
       token: '4AWpL9lhMYw.lzxO8vAJwXBIvTEhVbuI-sEA45eGk67Px04tzn9bWcU'
     });
+
   }
 
   render() {
-    return <ReactWebChat directLine={this.directLine} />;
+    return <ReactWebChat directLine={this.directLine} styleOptions={styleOptions} />;
   }
 }
