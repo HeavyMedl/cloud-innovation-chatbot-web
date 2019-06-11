f838574b6295ad34eebe921594c05010f03e1651
Kurt Medley <kurtdmedley@gmail.com>
2019-06-11 14:33:27 -0700

    Another test if this will deploy to Azure

---
+++ build/precache-manifest.524d58ed82d4409e125ff05f7df7bc71.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3399f028c2bcf91925ce30bb41c39ab4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8199a06bbc181409db5",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "f8199a06bbc181409db5",
+    "url": "/static/js/main.e9f46f8c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
