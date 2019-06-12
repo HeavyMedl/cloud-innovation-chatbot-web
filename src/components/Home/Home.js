1460b94c0a176a7ffd751a61729779ac192e91a2
Kurt Medley <kurtdmedley@gmail.com>
2019-06-12 16:52:30 -0700

    Removing useless variable, renaming exported functional component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -76,37 +76,7 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
-const footers = [
-  {
-    title: 'Company',
-    description: ['Team', 'History', 'Contact us', 'Locations']
-  },
-  {
-    title: 'Features',
-    description: [
-      'Cool stuff',
-      'Random feature',
-      'Team feature',
-      'Developer stuff',
-      'Another one'
-    ]
-  },
-  {
-    title: 'Resources',
-    description: [
-      'Resource',
-      'Resource name',
-      'Another resource',
-      'Final resource'
-    ]
-  },
-  {
-    title: 'Legal',
-    description: ['Privacy policy', 'Terms of use']
-  }
-];
-
-export default function Pricing() {
+export default function Home() {
   const classes = useStyles();
 
   return (
