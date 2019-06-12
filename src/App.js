6e5c1fc87074d65c3923e1f53464eae8e73beba5
Kurt Medley <kurtdmedley@gmail.com>
2019-06-12 10:26:57 -0700

    Adding home component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import './App.css';
-import WebChat from './components/WebChat/WebChat';
-import IphoneWrapper from './components/IphoneWrapper/IphoneWrapper';
+// import WebChat from './components/WebChat/WebChat';
+// import IphoneWrapper from './components/IphoneWrapper/IphoneWrapper';
+import Home from './components/Home/Home';
+import { CssBaseline } from '@material-ui/core';
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <IphoneWrapper>
-          <WebChat />
-        </IphoneWrapper>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Home />
+    </>
   );
 }
+
+// <div className='App'>
+// <header className='App-header'>
+//   <IphoneWrapper>
+//     // <WebChat />
+//   </IphoneWrapper>
+// </header>
+// </div>
 
 export default App;
