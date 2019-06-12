72eae28cc276e4ba885caae56ee46f8df3835264
Jana Uhrich <janamu@uw.edu>
2019-06-12 11:00:56 -0700

    added react router

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React from 'react';
 import './App.css';
-// import WebChat from './components/WebChat/WebChat';
-// import IphoneWrapper from './components/IphoneWrapper/IphoneWrapper';
+import WebChat from './components/WebChat/WebChat';
+import IphoneWrapper from './components/IphoneWrapper/IphoneWrapper';
 import Home from './components/Home/Home';
 import { CssBaseline } from '@material-ui/core';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <CssBaseline />
-      <Home />
-    </>
+    <Router>
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/web" component={WebChat} />
+        <Route path="/iphone" component={IphoneWrapper} />
+      </div>
+    </Router>
   );
 }
+
+
+//function App() {
+//  return (
+//    <>
+//      <CssBaseline />
+//      <Home />
+//    </>
+//  );
+//}
 
 // <div className='App'>
 // <header className='App-header'>
